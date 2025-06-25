1eb38e631e3e4f9fa029c1dac6044f744f6ae66b
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-06-25 11:41:22 +0400

    update

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -29,3 +29,52 @@ int	ft_putnbr(unsigned int num, int base)
 	count += ft_putchar(base_set[i]);
 	return (count);
 }
+
+int	ft_print_d(int nb)
+{
+	int		count = 0;
+
+	if (nb < 0)
+	{
+		count += ft_putchar('-');
+		if (nb == - 2147483648)
+		{
+			count += ft_putstr("2147483648");
+			return (count);
+		}
+		nb = -nb;
+	}
+	count += ft_putnbr((unsigned int)nb, 10);
+	return (count);
+}
+
+int	ft_printf(char *str, ...)
+{
+	va_list	ap;
+	int count = 0;
+	int i = 0;
+
+	va_start(ap, str);
+	if (str == NULL)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			if (str[i + 1] == 'd')
+				count += ft_print_d(va_arg(ar, int));
+			else if (str[i + 1] == 'x')
+				count += ft_putnbr(va_arg(ap, unsigned int), 16);
+			else if (str[i + 1] == 's')
+				count += ft_putstr(va_arg(arg, char *));
+			i++;
+		}
+		else
+		{
+			count += ft_putchar(str[i]);
+		}
+		i++;
+	}
+	va_end(ap);
+	return (count);
+}
