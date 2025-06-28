4bc04a12bb0e81a2aef4648473cde4f11b99fd96
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-06-28 19:14:02 +0400

    add

---
+++ ft_printf/test2.c
@@ -0,0 +1,94 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+int ft_putchar(char ch)
+{
+	return (write(1, &ch, 1));
+}
+
+int ft_putstr(char *str)
+{
+	int count = 0;
+	int i = 0;
+
+	if (str == NULL)
+		str = "(null)";
+	while (str[i])
+	{
+		count += ft_putchar(str[i]);
+		i++;
+	}
+	return (count);
+}
+
+int ft_putnbr(unsigned int nb, int base)
+{
+	int count = 0;
+	char *base_set = "0123456789abcdef";
+	int i = nb % base;
+
+	if (nb / base > 0)
+		ft_putnbr(nb / base, base);
+	count += ft_putchar(base_set[i]);
+	return (count);
+}
+
+int ft_print_d(int nb)
+{
+	int count = 0;
+	
+	if (nb < 0)
+	{
+		count += ft_putchar('-');
+		if (nb == -2147483648)
+		{
+			count += ft_putstr("2147483648");
+			return (count);
+		}
+		else
+		{
+			nb = -nb;
+		}
+	}
+	count += ft_putnbr((unsigned int)nb, 10);
+	return (count);
+}
+
+int ft_printf(char *str, ...)
+{
+	va_list ap;
+	int count = 0;
+	int i = 0;
+
+	va_start(ap, str);
+	if (str == NULL)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			if (str[i + 1] == 's')
+				count += ft_putstr(va_arg(ap, char *));
+			else if (str[i + 1] == 'x')
+				count += ft_putnbr(va_arg(ap, unsigned int), 16);
+			else if (str[i + 1] == 'd')
+				count += ft_print_d(va_arg(ap, int));
+			i++;
+		}
+		else
+		{
+			count += ft_putchar(str[i]);
+		}
+		i++;
+	}
+	va_end(ap);
+	return (count);
+}
+
+int main(void)
+{
+	ft_printf("test_%s_%d_%x\n", "str", -2147483648, 123);
+	printf("test_%s_%d_%x\n", "str", 123, 123);
+	return (0);
+}
